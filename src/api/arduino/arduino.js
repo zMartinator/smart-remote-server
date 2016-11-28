f866e4fc7b6ac11580e492f5603f7e7202cb2548
Zach Martinez <zach@martinezhome.org>
2016-11-27 22:18:04 -0500

    almost done

--- src/api/arduino/arduino.js
+++ src/api/arduino/arduino.js
@@ -52,7 +52,7 @@ export function power(request, response) {
   console.log(request.params.value);
   console.log("Here");
   //ip address of arduino/arduino/volumeUp
-  axios.get('http://192.168.0.20/arduino/power');
+  axios.get('http://192.168.0.20/arduino/'+request.params.value+'/PWR');
 
   respondWithResult(response)(5);
 }
@@ -61,7 +61,7 @@ export function volumeUp(request, response) {
   console.log(request.params.value);
   console.log("Here");
   //ip address of arduino/arduino/volumeUp
-  axios.get('http://192.168.0.20/arduino/vUp');
+  axios.get('http://192.168.0.20/arduino/'+request.params.value+'/vUp');
 
   respondWithResult(response)(5);
 }
@@ -71,7 +71,7 @@ export function volumeDown(request, response) {
   console.log(request.params.value);
   console.log("Here");
   //ip address of arduino/arduino/volumeUp
-  axios.get('http://192.168.0.20/arduino/vDown');
+  axios.get('http://192.168.0.20/arduino/'+request.params.value+'/vDown');
 
   respondWithResult(response)(5);
 }
@@ -80,7 +80,7 @@ export function mute(request, response) {
   console.log(request.params.value);
   console.log("Here");
   //ip address of arduino/arduino/volumeUp
-  axios.get('http://192.168.0.20/arduino/mute');
+  axios.get('http://192.168.0.20/arduino/'+request.params.value+'/MUTE');
 
   respondWithResult(response)(5);
 }
@@ -89,7 +89,7 @@ export function channelUp(request, response) {
   console.log(request.params.value);
   console.log("Here");
   //ip address of arduino/arduino/volumeUp
-  axios.get('http://192.168.0.20/arduino/cUp');
+  axios.get('http://192.168.0.20/arduino/'+request.params.value+'/cUp');
 
   respondWithResult(response)(5);
 }
@@ -98,7 +98,178 @@ export function channelDown(request, response) {
   console.log(request.params.value);
   console.log("Here");
   //ip address of arduino/arduino/volumeUp
-  axios.get('http://192.168.0.20/arduino/cDown');
+  axios.get('http://192.168.0.20/arduino/'+request.params.value+'/cDown');
+
+  respondWithResult(response)(5);
+}
+
+export function up(request, response) {
+  console.log(request.params.value);
+  console.log("up");
+  //ip address of arduino/arduino/volumeUp
+  axios.get('http://192.168.0.20/arduino/'+request.params.value+'/UP');
+
+  respondWithResult(response)(5);
+}
+
+export function down(request, response) {
+  console.log(request.params.value);
+  console.log("down");
+  //ip address of arduino/arduino/volumeUp
+  axios.get('http://192.168.0.20/arduino/'+request.params.value+'/DOWN');
+
+  respondWithResult(response)(5);
+}
+
+export function left(request, response) {
+  console.log(request.params.value);
+  console.log("left");
+  //ip address of arduino/arduino/volumeUp
+  axios.get('http://192.168.0.20/arduino/'+request.params.value+'/LEFT');
+
+  respondWithResult(response)(5);
+}
+
+export function right(request, response) {
+  console.log(request.params.value);
+  console.log("right");
+  //ip address of arduino/arduino/volumeUp
+  axios.get('http://192.168.0.20/arduino/'+request.params.value+'/RIGHT');
+
+  respondWithResult(response)(5);
+}
+
+export function enter(request, response) {
+  console.log(request.params.value);
+  console.log("enter");
+  //ip address of arduino/arduino/volumeUp
+  axios.get('http://192.168.0.20/arduino/'+request.params.value+'/ENTER');
+
+  respondWithResult(response)(5);
+}
+
+export function menu(request, response) {
+  console.log(request.params.value);
+  console.log("menu");
+  //ip address of arduino/arduino/volumeUp
+  axios.get('http://192.168.0.20/arduino/'+request.params.value+'/MENU');
+
+  respondWithResult(response)(5);
+}
+
+export function input(request, response) {
+  console.log(request.params.value);
+  console.log("input");
+  //ip address of arduino/arduino/volumeUp
+  axios.get('http://192.168.0.20/arduino/'+request.params.value+'/INPUT');
+
+  respondWithResult(response)(5);
+}
+
+export function exit(request, response) {
+  console.log(request.params.value);
+  console.log("exit");
+  //ip address of arduino/arduino/volumeUp
+  axios.get('http://192.168.0.20/arduino/'+request.params.value+'/EXIT');
+
+  respondWithResult(response)(5);
+}
+
+export function last(request, response) {
+  console.log(request.params.value);
+  console.log("last");
+  //ip address of arduino/arduino/volumeUp
+  axios.get('http://192.168.0.20/arduino/'+request.params.value+'/LAST');
+
+  respondWithResult(response)(5);
+}
+
+export function one(request, response) {
+  console.log(request.params.value);
+  console.log("one");
+  //ip address of arduino/arduino/volumeUp
+  axios.get('http://192.168.0.20/arduino/'+request.params.value+'/1');
+
+  respondWithResult(response)(5);
+}
+
+export function two(request, response) {
+  console.log(request.params.value);
+  console.log("two");
+  //ip address of arduino/arduino/volumeUp
+  axios.get('http://192.168.0.20/arduino/'+request.params.value+'/2');
+
+  respondWithResult(response)(5);
+}
+
+export function three(request, response) {
+  console.log(request.params.value);
+  console.log("three");
+  //ip address of arduino/arduino/volumeUp
+  axios.get('http://192.168.0.20/arduino/'+request.params.value+'/3');
+
+  respondWithResult(response)(5);
+}
+
+export function four(request, response) {
+  console.log(request.params.value);
+  console.log("four");
+  //ip address of arduino/arduino/volumeUp
+  axios.get('http://192.168.0.20/arduino/'+request.params.value+'/4');
+
+  respondWithResult(response)(5);
+}
+
+export function five(request, response) {
+  console.log(request.params.value);
+  console.log("five");
+  //ip address of arduino/arduino/volumeUp
+  axios.get('http://192.168.0.20/arduino/'+request.params.value+'/5');
+
+  respondWithResult(response)(5);
+}
+
+export function six(request, response) {
+  console.log(request.params.value);
+  console.log("six");
+  //ip address of arduino/arduino/volumeUp
+  axios.get('http://192.168.0.20/arduino/'+request.params.value+'/6');
+
+  respondWithResult(response)(5);
+}
+
+export function seven(request, response) {
+  console.log(request.params.value);
+  console.log("seven");
+  //ip address of arduino/arduino/volumeUp
+  axios.get('http://192.168.0.20/arduino/'+request.params.value+'/7');
+
+  respondWithResult(response)(5);
+}
+
+export function eight(request, response) {
+  console.log(request.params.value);
+  console.log("eight");
+  //ip address of arduino/arduino/volumeUp
+  axios.get('http://192.168.0.20/arduino/'+request.params.value+'/8');
+
+  respondWithResult(response)(5);
+}
+
+export function nine(request, response) {
+  console.log(request.params.value);
+  console.log("nine");
+  //ip address of arduino/arduino/volumeUp
+  axios.get('http://192.168.0.20/arduino/'+request.params.value+'/9');
+
+  respondWithResult(response)(5);
+}
+
+export function zero(request, response) {
+  console.log(request.params.value);
+  console.log("zero");
+  //ip address of arduino/arduino/volumeUp
+  axios.get('http://192.168.0.20/arduino/'+request.params.value+'/0');
 
   respondWithResult(response)(5);
 }
--- src/api/arduino/index.js
+++ src/api/arduino/index.js
@@ -1,14 +1,35 @@
 import express from 'express';
-import { power, volumeUp, volumeDown, mute, channelUp, channelDown } from './arduino';
+import { power, volumeUp, volumeDown, mute, channelUp, channelDown, up, down,
+  right, left, enter, menu, input, exit, last, one, two, three, four, five,
+  six, seven, eight, nine, zero } from './arduino';
 
 
 const router = express.Router();
 
-router.post('/power', power);
-router.post('/vUp', volumeUp);
-router.post('/vDown', volumeDown);
-router.post('/mute', mute);
-router.post('/cUp', channelUp);
-router.post('/cDown', channelDown);
+router.post('/power/:value', power);
+router.post('/vUp/:value', volumeUp);
+router.post('/vDown/:value', volumeDown);
+router.post('/mute/:value', mute);
+router.post('/cUp/:value', channelUp);
+router.post('/cDown/:value', channelDown);
+router.post('/up/:value', up);
+router.post('/right/:value', right);
+router.post('/down/:value', down);
+router.post('/left/:value', left);
+router.post('/enter/:value', enter);
+router.post('/menu/:value', menu);
+router.post('/input/:value', input);
+router.post('/exit/:value', exit);
+router.post('/last/:value', last);
+router.post('/one/:value', one);
+router.post('/two/:value', two);
+router.post('/three/:value', three);
+router.post('/four/:value', four);
+router.post('/five/:value', five);
+router.post('/six/:value', six);
+router.post('/seven/:value', seven);
+router.post('/eight/:value', eight);
+router.post('/nine/:value', nine);
+router.post('/zero/:value', zero);
 
 export default router;
